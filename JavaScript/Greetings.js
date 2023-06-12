d69af97c1bbdbba32718ee9d79f5a8608320a5fd
Mike Tray <miketraynor165@gmail.com>
2023-06-12 14:48:57 -0500

    commit

--- JavaScript/Greetings.js
+++ JavaScript/Greetings.js
@@ -11,3 +11,19 @@ if (hourNow >=18) {
     greeting = 'Welcome!'
 }
 document.write(greeting);
+
+//Create Three variables to store the information needed.
+var price;
+var quantity;
+var total;
+
+//Assign values to the price and quantity variables.
+price = 5;
+quantity = 14;
+//Calculate the total by multiplying the price by quantity.
+total = price * quantity;
+
+//get the element with an id of cost.
+
+var el = document.getElementById('cost');
+el.textContent = '$' + total;
